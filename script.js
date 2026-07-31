4e3c0a15e730b620a1f27c9483a4c0c2d3ad97ca
mudrikmusemma22-ux <mudrikmusemma22@gmail.com>
2026-07-31 19:03:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ resize();
 const player = {
     x: 100,
     width: 40,
-    height: 80,
     speed: 5,
     dx: 0
 };
@@ -50,10 +49,77 @@ function update() {
 }
 
 
+// Wolken
+function drawCloud(x, y, size) {
+
+    ctx.fillStyle = "#f5ead2";
+
+    ctx.beginPath();
+
+    ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.arc(x + size, y - 10, size * 1.2, 0, Math.PI * 2);
+    ctx.arc(x + size * 2, y, size, 0, Math.PI * 2);
+
+    ctx.fill();
+}
+
+
+// Berge
+function drawMountain(x, y, width, height) {
+
+    ctx.fillStyle = "#b8a58a";
+
+    ctx.beginPath();
+
+    ctx.moveTo(x, y);
+
+    ctx.lineTo(
+        x + width / 2,
+        y - height
+    );
+
+    ctx.lineTo(
+        x + width,
+        y
+    );
+
+    ctx.closePath();
+
+    ctx.fill();
+
+
+    // Schnee auf Berg
+    ctx.fillStyle = "#fff4dd";
+
+    ctx.beginPath();
+
+    ctx.moveTo(
+        x + width / 2,
+        y - height
+    );
+
+    ctx.lineTo(
+        x + width / 2 - 45,
+        y - height + 70
+    );
+
+    ctx.lineTo(
+        x + width / 2 + 45,
+        y - height + 70
+    );
+
+    ctx.closePath();
+
+    ctx.fill();
+}
+
+
 // Zeichnen
 function draw() {
 
+
     // golden brown Himmel
+
     let sky = ctx.createLinearGradient(
         0,
         0,
@@ -66,6 +132,7 @@ function draw() {
     sky.addColorStop(1, "#f0dfbd");
 
     ctx.fillStyle = sky;
+
     ctx.fillRect(
         0,
         0,
@@ -74,190 +141,147 @@ function draw() {
     );
 
 
-   // Wolken (gemalter Stil)
+    // Wolken
 
-function drawCloud(x, y, size) {
+    drawCloud(150, 120, 30);
+    drawCloud(600, 160, 45);
+    drawCloud(1000, 100, 35);
 
-    ctx.fillStyle = "#f5ead2";
 
-    ctx.beginPath();
+    // Berge
 
-    ctx.arc(
-        x,
-        y,
-        size,
+    drawMountain(
+        50,
+        canvas.height - 120,
+        500,
+        300
+    );
+
+    drawMountain(
+        650,
+        canvas.height - 120,
+        600,
+        400
+    );
+
+
+    // Schnee Boden
+
+    ctx.fillStyle = "#e8dfc8";
+
+    ctx.fillRect(
         0,
-        Math.PI * 2
+        canvas.height - 120,
+        canvas.width,
+        120
     );
 
-    ctx.arc(
-        x + size,
-        y - 10,
-        size * 1.2,
-        0,
-        Math.PI * 2
-    );
 
-    ctx.arc(
-        x + size * 2,
-        y,
-        size,
-        0,
-        Math.PI * 2
-    );
-
-    ctx.fill();
-}
-
-
-// wolken am himmel
-
-drawCloud(200, 120, 30);
-drawCloud(600, 180, 45);
-drawCloud(1000, 100, 35);
-
-    
-    // Berge (gemalter Stil)
-
-function drawMountain(x, y, width, height) {
-
-    // Berg
-    ctx.fillStyle = "#b8a58a";
-
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(
-        x + width / 2,
-        y - height
-    );
-    ctx.lineTo(
-        x + width,
-        y
-    );
-    ctx.closePath();
-    ctx.fill();
-
-
-    // Schnee auf der Spitze
-    ctx.fillStyle = "#fff4dd";
-
-    ctx.beginPath();
-    ctx.moveTo(
-        x + width / 2,
-        y - height
-    );
-    ctx.lineTo(
-        x + width / 2 - 40,
-        y - height + 60
-    );
-    ctx.lineTo(
-        x + width / 2 + 40,
-        y - height + 60
-    );
-    ctx.closePath();
-
-    ctx.fill();
-}
-
-
-// hintergrund berge
-
-drawMountain(
-    100,
-    canvas.height - 120,
-    500,
-    300
-);
-
-drawMountain(
-    700,
-    canvas.height - 120,
-    600,
-    400
-);
     // Doodle Ritter
 
     let x = player.x;
-    let y = canvas.height - 180;
+    let y = canvas.height - 200;
 
 
     // Umhang
+
     ctx.fillStyle = "#7d3030";
+
     ctx.beginPath();
+
     ctx.moveTo(x + 5, y + 30);
-    ctx.lineTo(x - 25, y + 85);
+    ctx.lineTo(x - 20, y + 90);
     ctx.lineTo(x + 15, y + 75);
+
     ctx.closePath();
+
     ctx.fill();
 
 
     // Rüstung
+
     ctx.fillStyle = "#777";
+
     ctx.fillRect(
         x,
         y + 35,
         40,
-        45
+        50
     );
 
 
     // Helm
+
     ctx.fillStyle = "#aaa";
+
     ctx.beginPath();
+
     ctx.arc(
         x + 20,
-        y + 25,
+        y + 20,
         22,
         0,
         Math.PI * 2
     );
+
     ctx.fill();
 
 
     // Visier
+
     ctx.fillStyle = "#333";
+
     ctx.fillRect(
         x + 5,
-        y + 20,
+        y + 15,
         30,
         6
     );
 
 
     // Schwert
+
     ctx.strokeStyle = "#222";
     ctx.lineWidth = 5;
 
     ctx.beginPath();
+
     ctx.moveTo(
         x + 40,
         y + 55
     );
+
     ctx.lineTo(
         x + 70,
         y + 20
     );
+
     ctx.stroke();
 
 
-    // Position
+    // Testanzeige
+
     ctx.fillStyle = "#000";
     ctx.font = "20px Arial";
+
     ctx.fillText(
         "doodle x: " + Math.floor(player.x),
         20,
         40
     );
+
 }
 
 
 // Spielschleife
+
 function gameLoop() {
 
     update();
     draw();
 
     requestAnimationFrame(gameLoop);
+
 }
 
 
