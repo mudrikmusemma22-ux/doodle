25dce9d42b2ae15ca0ac9b79a2fcca579e92179f
mudrikmusemma22-ux <mudrikmusemma22@gmail.com>
2026-07-31 23:47:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,144 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+
 // größe
 
 function resize() {
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
 }
 
 window.addEventListener("resize", resize);
 resize();
 
-// spiel
+
+// spieler
+
+const player = {
+
+    x: 300,
+    y: 300,
+    speed: 5
+
+};
+
+
+// tasten
+
+const keys = {};
+
+window.addEventListener("keydown", (e) => {
+
+    keys[e.key.toLowerCase()] = true;
+
+});
+
+
+window.addEventListener("keyup", (e) => {
+
+    keys[e.key.toLowerCase()] = false;
+
+});
+
+
+// bewegung
 
 function update() {
-const player = {
-    x: 300,
-    y: 300
-};
+
+
+    if(keys["a"]) {
+
+        player.x -= player.speed;
+
+    }
+
+
+    if(keys["d"]) {
+
+        player.x += player.speed;
+
+    }
+
+
+}
+
+
+// zeichnen
 
 function draw() {
 
-    // hintergrund
-    ctx.fillStyle = "#f2e5c5";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // test
-    ctx.fillStyle = "#222";
-    ctx.font = "28px Arial";
-    ctx.fillText("doodle engine v2", 30, 50);
+    // golden brown hintergrund
+
+    let sky = ctx.createLinearGradient(
+        0,
+        0,
+        0,
+        canvas.height
+    );
+
+
+    sky.addColorStop(0, "#a87545");
+    sky.addColorStop(1, "#f0dfbd");
+
+
+    ctx.fillStyle = sky;
+
+    ctx.fillRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+
+
+    // spieler körper
+
+    ctx.fillStyle = "#777";
+
+    ctx.fillRect(
+        player.x,
+        player.y,
+        45,
+        60
+    );
+
+
+    // kopf
+
+    ctx.fillStyle = "#aaa";
+
+    ctx.beginPath();
+
+    ctx.arc(
+        player.x + 22,
+        player.y - 10,
+        22,
+        0,
+        Math.PI * 2
+    );
+
+    ctx.fill();
+
 
 }
+
+
+// loop
 
 function gameLoop() {
 
     update();
+
     draw();
 
     requestAnimationFrame(gameLoop);
 
 }
+
 
 gameLoop();
