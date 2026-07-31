cc6fae6b16a06eb37535114e6fa801ee7bd745a3
mudrikmusemma22-ux <mudrikmusemma22@gmail.com>
2026-07-31 21:52:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -358,7 +358,6 @@ function update(){
 
 }
 
-
 function drawBrush(){
 
     if(mouse.down && brush){
@@ -371,6 +370,7 @@ function drawBrush(){
 
         });
 
+    }
 
 
     for(let d of drawings){
@@ -398,6 +398,63 @@ function drawBrush(){
         ctx.strokeStyle = "rgba(120,20,20,0.35)";
         ctx.lineWidth = 1;
 
+        ctx.beginPath();
+
+        ctx.moveTo(
+            d.x - 4,
+            d.y - 4
+        );
+
+
+        ctx.lineTo(
+            d.x + 6,
+            d.y + 5
+        );
+
+
+        ctx.stroke();
+
+    }
+
+}function drawBrush(){
+
+    if(mouse.down && brush){
+
+        drawings.push({
+
+            x: mouse.x,
+            y: mouse.y,
+            size: Math.random() * 4 + 3
+
+        });
+
+    }
+
+
+    for(let d of drawings){
+
+        // hauptstrich
+
+        ctx.fillStyle = "rgba(190,40,40,0.7)";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            d.x,
+            d.y,
+            d.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+
+
+        // kleine buntstift kratzer
+
+        ctx.strokeStyle = "rgba(120,20,20,0.35)";
+        ctx.lineWidth = 1;
 
         ctx.beginPath();
 
@@ -413,12 +470,533 @@ function drawBrush(){
         );
 
 
-            ctx.stroke();
+        ctx.stroke();
+
+    }
+
+}function drawBrush(){
+
+    if(mouse.down && brush){
+
+        drawings.push({
+
+            x: mouse.x,
+            y: mouse.y,
+            size: Math.random() * 4 + 3
+
+        });
+
+    }
+
+
+    for(let d of drawings){
+
+        // hauptstrich
+
+        ctx.fillStyle = "rgba(190,40,40,0.7)";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            d.x,
+            d.y,
+            d.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+
+
+        // kleine buntstift kratzer
+
+        ctx.strokeStyle = "rgba(120,20,20,0.35)";
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(
+            d.x - 4,
+            d.y - 4
+        );
+
+
+        ctx.lineTo(
+            d.x + 6,
+            d.y + 5
+        );
+
+
+        ctx.stroke();
+
+    }
+
+}function drawBrush(){
+
+    if(mouse.down && brush){
+
+        drawings.push({
+
+            x: mouse.x,
+            y: mouse.y,
+            size: Math.random() * 4 + 3
+
+        });
+
+    }
+
+
+    for(let d of drawings){
+
+        // hauptstrich
+
+        ctx.fillStyle = "rgba(190,40,40,0.7)";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            d.x,
+            d.y,
+            d.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+
+
+        // kleine buntstift kratzer
+
+        ctx.strokeStyle = "rgba(120,20,20,0.35)";
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(
+            d.x - 4,
+            d.y - 4
+        );
+
+
+        ctx.lineTo(
+            d.x + 6,
+            d.y + 5
+        );
+
+
+        ctx.stroke();
+
+    }
+
+}function drawBrush(){
+
+    if(mouse.down && brush){
+
+        drawings.push({
+
+            x: mouse.x,
+            y: mouse.y,
+            size: Math.random() * 4 + 3
+
+        });
+
+    }
+
+
+    for(let d of drawings){
+
+        // hauptstrich
+
+        ctx.fillStyle = "rgba(190,40,40,0.7)";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            d.x,
+            d.y,
+            d.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+
+
+        // kleine buntstift kratzer
+
+        ctx.strokeStyle = "rgba(120,20,20,0.35)";
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(
+            d.x - 4,
+            d.y - 4
+        );
+
+
+        ctx.lineTo(
+            d.x + 6,
+            d.y + 5
+        );
+
+
+        ctx.stroke();
+
+    }
+
+}function drawBrush(){
+
+    if(mouse.down && brush){
+
+        drawings.push({
+
+            x: mouse.x,
+            y: mouse.y,
+            size: Math.random() * 4 + 3
+
+        });
+
+    }
+
+
+    for(let d of drawings){
+
+        // hauptstrich
+
+        ctx.fillStyle = "rgba(190,40,40,0.7)";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            d.x,
+            d.y,
+            d.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+
+
+        // kleine buntstift kratzer
+
+        ctx.strokeStyle = "rgba(120,20,20,0.35)";
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(
+            d.x - 4,
+            d.y - 4
+        );
+
+
+        ctx.lineTo(
+            d.x + 6,
+            d.y + 5
+        );
+
+
+        ctx.stroke();
+
+    }
+
+}function drawBrush(){
+
+    if(mouse.down && brush){
+
+        drawings.push({
+
+            x: mouse.x,
+            y: mouse.y,
+            size: Math.random() * 4 + 3
+
+        });
+
+    }
+
+
+    for(let d of drawings){
+
+        // hauptstrich
+
+        ctx.fillStyle = "rgba(190,40,40,0.7)";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            d.x,
+            d.y,
+            d.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+
+
+        // kleine buntstift kratzer
+
+        ctx.strokeStyle = "rgba(120,20,20,0.35)";
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(
+            d.x - 4,
+            d.y - 4
+        );
+
+
+        ctx.lineTo(
+            d.x + 6,
+            d.y + 5
+        );
+
+
+        ctx.stroke();
+
+    }
+
+}function drawBrush(){
+
+    if(mouse.down && brush){
+
+        drawings.push({
+
+            x: mouse.x,
+            y: mouse.y,
+            size: Math.random() * 4 + 3
+
+        });
+
+    }
+
+
+    for(let d of drawings){
+
+        // hauptstrich
+
+        ctx.fillStyle = "rgba(190,40,40,0.7)";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            d.x,
+            d.y,
+            d.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+
+
+        // kleine buntstift kratzer
+
+        ctx.strokeStyle = "rgba(120,20,20,0.35)";
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(
+            d.x - 4,
+            d.y - 4
+        );
+
+
+        ctx.lineTo(
+            d.x + 6,
+            d.y + 5
+        );
+
+
+        ctx.stroke();
+
+    }
+
+}function drawBrush(){
+
+    if(mouse.down && brush){
+
+        drawings.push({
+
+            x: mouse.x,
+            y: mouse.y,
+            size: Math.random() * 4 + 3
+
+        });
+
+    }
+
+
+    for(let d of drawings){
+
+        // hauptstrich
+
+        ctx.fillStyle = "rgba(190,40,40,0.7)";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            d.x,
+            d.y,
+            d.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+
+
+        // kleine buntstift kratzer
+
+        ctx.strokeStyle = "rgba(120,20,20,0.35)";
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(
+            d.x - 4,
+            d.y - 4
+        );
+
+
+        ctx.lineTo(
+            d.x + 6,
+            d.y + 5
+        );
+
+
+        ctx.stroke();
+
+    }
+
+}function drawBrush(){
+
+    if(mouse.down && brush){
+
+        drawings.push({
+
+            x: mouse.x,
+            y: mouse.y,
+            size: Math.random() * 4 + 3
+
+        });
+
+    }
+
+
+    for(let d of drawings){
+
+        // hauptstrich
+
+        ctx.fillStyle = "rgba(190,40,40,0.7)";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            d.x,
+            d.y,
+            d.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+
+
+        // kleine buntstift kratzer
+
+        ctx.strokeStyle = "rgba(120,20,20,0.35)";
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(
+            d.x - 4,
+            d.y - 4
+        );
+
+
+        ctx.lineTo(
+            d.x + 6,
+            d.y + 5
+        );
+
+
+        ctx.stroke();
+
+    }
+
+}function drawBrush(){
+
+    if(mouse.down && brush){
+
+        drawings.push({
+
+            x: mouse.x,
+            y: mouse.y,
+            size: Math.random() * 4 + 3
+
+        });
+
+    }
+
+
+    for(let d of drawings){
+
+        // hauptstrich
+
+        ctx.fillStyle = "rgba(190,40,40,0.7)";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            d.x,
+            d.y,
+            d.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+
+
+        // kleine buntstift kratzer
+
+        ctx.strokeStyle = "rgba(120,20,20,0.35)";
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(
+            d.x - 4,
+            d.y - 4
+        );
+
+
+        ctx.lineTo(
+            d.x + 6,
+            d.y + 5
+        );
+
+
+        ctx.stroke();
 
     }
 
 }
-
  
 // zeichnen
 
